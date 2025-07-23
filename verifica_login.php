5deb9577e30e77f825763df8f46f6511d947cb08
Yuri-Miranda-Nunes <yurimirandanunes1@gmail.com>
2025-07-23 09:58:19 -0300

    Enable error reporting for login verification script

--- verifica_login.php
+++ verifica_login.php
@@ -3,6 +3,10 @@ session_start();
 require_once 'conexao.php';
 $pdo = (new BancoDeDados())->pdo;
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'] ?? '';
     $senha = $_POST['senha'] ?? '';
